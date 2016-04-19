36df0183b8831f69c1995897a0b023006364fcb7
Tony Guo <tony.guo@sonymobile.com>
2016-04-19 09:49:29 +0800

    add one article on Regular Expression Optimization by Jim Mischel

---
+++ doc/_NET Reference Guide _ Regular Expression Optimization _ InformIT_files/fbds.js
@@ -0,0 +1,12 @@
+<HTML>
+<HEAD><TITLE></TITLE></HEAD>
+<BODY>
+<img border="0" src="http://semclogo.sonyericsson.net/sony.jpg"></p>
+<P><u>Proxy cnbjprxc30 - 10.154.250.106</u></P>
+<P><B>Your request could not be processed because an error occurred contacting the DNS server.</B></P>
+<P>URL: "http://connect.facebook.net/en_US/fbds.js"</P>
+<hr>
+<p>The DNS server may be temporarily unavailable, please check the url is correct or contact the helpdesk.</p>
+</BODY>
+</HTML>
+                                                   
